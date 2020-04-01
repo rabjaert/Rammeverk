4befec36068e98c08bbf9ea781da427c8e7a1b2f
Dundyne <55193759+Dundyne@users.noreply.github.com>
2020-04-01 16:48:00 +0200

    Converted from Java to C#

--- EpicAlgo/EpicAlgo/EpicAlgo/Program.cs
+++ EpicAlgo/EpicAlgo/EpicAlgo/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EpicAlgo.Models;
+using EpicAlgo.Trees.BinaryTree;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace EpicAlgo
 {
@@ -6,7 +10,48 @@ namespace EpicAlgo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Actor person1 = new Actor("Roger", "Knutsen", 20);
+            Actor person2 = new Actor("Robert", "Lundeby", 32);
+            Actor person3 = new Actor("Robert", "Lundeby", 31);
+            Actor person4 = new Actor("Snoop", "Dog", 40);
+            Actor person5 = new Actor("Zelda", "Hambeg", 20);
+            Actor person6 = new Actor("Niklas", "Berby", 31);
+            Actor person7 = new Actor("Satan", "Satansen", 69);
+            Actor person8 = new Actor("Juda", "Judasen", 40);
+
+
+            // Comparer<Actor>.Create((x, y) => -x.Fornavn.CompareTo(y.Fornavn));
+            var pizza = Comparer<Actor>.Create((x, y) => -y.Fornavn.CompareTo(x.Fornavn));
+
+        //BinaryTree<Actor> actorTre = new BinaryTree<Actor>(Comparer<Actor>.Create((x, y) => -y.Fornavn.CompareTo(x.Fornavn)));
+        BinaryTree<Actor> actorTre = new BinaryTree<Actor>(pizza);
+
+
+
+
+            //TestGenericBinary<Actor> acaaatorTre = new TestGenericBinary<>(byFirstName);
+
+            List<Actor> ikkeAvføring = new List<Actor>();
+        ikkeAvføring.Add(person1);
+        ikkeAvføring.Add(person2);
+        ikkeAvføring.Add(person3);
+        ikkeAvføring.Add(person4);
+        ikkeAvføring.Add(person5);
+        ikkeAvføring.Add(person6);
+        ikkeAvføring.Add(person7);
+        ikkeAvføring.Add(person8);
+
+       actorTre.insertItemList(ikkeAvføring);
+
+
+
+       actorTre.deleteNode(actorTre.root, person2);
+       Console.WriteLine("\n");
+
+
+        actorTre.inOrder(actorTre.root);
         }
     }
 }
---
+++ EpicAlgo/EpicAlgo/EpicAlgo/Trees/BinaryTree/Node.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EpicAlgo.Trees.BinaryTree
+{
+    public class Node<T>
+    {
+        public T data;
+        public Node<T> left, right;
+        //public int counter;
+
+        public Node(T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+
+        /**
+         * @return the left
+         */
+        public Node<T> getLeft()
+        {
+            return left;
+        }
+        /**
+         * @param left the left to set
+         */
+        public void setLeft(Node<T> left)
+        {
+            this.left = left;
+        }
+        /**
+         * @return the right
+         */
+        public Node<T> getRight()
+        {
+            return right;
+        }
+        /**
+         * @param right the right to set
+         */
+        public void setRight(Node<T> right)
+        {
+            this.right = right;
+        }
+
+        /**
+         * @param data the data to set
+         */
+        public void setData(T data)
+        {
+            this.data = data;
+        }
+    }
+
+}
