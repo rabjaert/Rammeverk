f9e4ac5edee3c4be18af70826cff4aff0a7adb92
Robert <55789494+rabjaert@users.noreply.github.com>
2020-04-02 12:46:16 +0200

    Update IHashTable.cs

--- EpicAlgo/EpicAlgo/EpicAlgo/Interfaces/IHashTable.cs
+++ EpicAlgo/EpicAlgo/EpicAlgo/Interfaces/IHashTable.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace EpicAlgo.Interfaces
 {
-    interface IHashTable
+    interface IHashTable<Key, Type> : IEnumerable<KeyValuePair<Key, Type>>
     {
+        //nice
+        void ResizeIfRequires();
+
+        int GetIndex();
+        void Add();
+        int FindIndexOfKey();
+        bool Contains();
+        void Remove();
+        void Clear();
+
     }
 }
