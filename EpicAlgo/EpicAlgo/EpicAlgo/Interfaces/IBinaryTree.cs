871cfb6b5fdd2015490692e452cad8ff1fcb5ce6
Robert <55789494+rabjaert@users.noreply.github.com>
2020-04-03 13:05:40 +0200

    La til et interface

---
+++ EpicAlgo/EpicAlgo/EpicAlgo/Interfaces/IBinaryTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EpicAlgo.Interfaces
+{
+    public interface IBinaryTree<T>
+    {
+
+        public IBinaryNode<T> DeleteNode(IBinaryNode<T> root, T data);
+
+        public T MinValue(IBinaryNode<T> node);
+
+        public IBinaryNode<T> InsertNode(IBinaryNode<T> node, T data);
+
+        public void InsertItem(T word);
+
+        public void InsertItemIntoArray(T[] word);
+
+        public void InsertItemIntoList(List<T> data);
+
+        public Boolean Contains(IBinaryNode<T> root, T data);
+
+        public void PrintInOrder(IBinaryNode<T> root);
+
+        public void PrintPreOrder(IBinaryNode<T> root);
+
+        public void PrintPostOrder(IBinaryNode<T> root);
+
+
+
+
+        }
+}
